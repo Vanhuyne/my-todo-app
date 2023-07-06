fab3591547aa30a26bf39bb0d421eba41036949e
Vanhuyne <106498928+Vanhuyne@users.noreply.github.com>
2023-07-06 12:26:16 +0700

    Update db.js

--- server/db.js
+++ server/db.js
@@ -3,9 +3,9 @@ const Pool = require('pg').Pool;
 require('dotenv').config();
 
 const pool = new Pool({
-    user: "postgres",
-    password: "1999",
-    host: "localhost",
+    user: "QmyTEwABg0CkamXk",
+    password: "SRuHviEJ4yqMltKJ",
+    host: "todoapp-d5mme-postgresql.todoapp-d5mme.svc.cluster.local",
     port: 5432,
     database: 'todoapp'
 })
